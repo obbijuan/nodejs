d97d21ab8e0e75a4a31b2d7a6233757a8fdda8fa
obbijuan <juan.garces@outlook.com>
2019-01-03 02:58:28 -0300

    added yargs commands

--- tareas/app.js
+++ tareas/app.js
@@ -1,16 +1,17 @@
-const argv = require('yargs').argv;
+//const argv = require('yargs').argv;
+const argv = require('./config/yargs').argv;
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear Nota!')
+        console.log('Crear Tarea!')
         break;
     case 'listar':
-        console.log('Mostrar todas las Notas!')
+        console.log('Mostrar todas las Tareas!')
         break;
     case 'actualizar':
-        console.log('Actualiza una Nota!')
+        console.log('Actualiza una Tarea!')
         break;
     default:
         console.log('Comando no reconocido!');
---
+++ tareas/config/yargs.js
@@ -0,0 +1,24 @@
+const argv = require('yargs')
+    .command('crear', 'Crea una tarea',{
+        descripcion : {
+            demand:true,
+            alias:'d'
+        }
+    })
+    .command('actualizar', 'Actualiza una tarea',{
+        descripcion : {
+            demand:true,
+            alias:'d'
+        },
+        completado : {
+            default:true,
+            alias:'c',
+            desc: 'Marca como completado o pendiente una tarea.'
+        }
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
