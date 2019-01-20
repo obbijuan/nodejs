d4de27d0f45911dc1e4dc1c055c9989f722df21e
obbijuan <juan.garces@outlook.com>
2019-01-20 01:21:23 -0300

    small fixes

--- tareas/config/yargs.js
+++ tareas/config/yargs.js
@@ -1,27 +1,23 @@
+const descripcion = {
+    demand:true,
+    alias:'d'
+}
+const completado = {
+    default:true,
+    alias:'c',
+    desc: 'Marca como completado o pendiente una tarea.'
+}
+
 const argv = require('yargs')
     .command('crear', 'Crea una tarea',{
-        descripcion : {
-            demand:true,
-            alias:'d'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza una tarea',{
-        descripcion : {
-            demand:true,
-            alias:'d'
-        },
-        completado : {
-            default:true,
-            alias:'c',
-            desc: 'Marca como completado o pendiente una tarea.'
-        }
+        descripcion,
+        completado
     })
     .command('borrar', 'Borrar una tarea',{
-        descripcion : {
-            demand:true,
-            alias:'d',
-            desc: 'Marca como completado o pendiente una tarea.'
-        }
+        descripcion
     })
     .help()
     .argv;
