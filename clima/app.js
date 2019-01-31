cf517c3a98eefd99ba1a9c688c02ccbdb0e04573
obbiejuan <juan.garces@outlook.com>
2019-01-31 14:42:00 -0300

    refactoring

--- clima/app.js
+++ clima/app.js
@@ -1,15 +1,8 @@
-const axios = require('axios');
 const argv = require('./config/yargs').argv;
+const lugar = require('./lugar/lugar');
 
-let encodeUrl = encodeURI(argv.direccion);
-axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeUrl}&key=AIzaSyDSPDpkFznGgzzBSsYvTq_sj0T0QCHRgwM`)
-  .then( resp => {
-
-      let location = resp.data.results[0];
-      let coord = location.geometry.location;
-
-      console.log('Direccion: ', location.formatted_address)
-      console.log('Latitud: ', coord.lat)
-      console.log('Longitud: ', coord.lng)
-  })
-  .catch( e => console.log('Error!', e));
+lugar.getLugarLatLng(argv.direccion)
+        .then(resp => {
+            console.log(resp);
+        })
+        .catch(e => console.log(e))
