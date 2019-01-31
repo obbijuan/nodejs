dc9833ff0a4724758a885fdfc0d616ecffd94e66
obbiejuan <juan.garces@outlook.com>
2019-01-31 14:21:22 -0300

    bugs fixed 2

--- clima/app.js
+++ clima/app.js
@@ -1,18 +1,10 @@
 const axios = require('axios');
+const argv = require('./config/yargs').argv;
 
-const argv = require('yargs').options({
-    direccion   :{
-        alias   : 'd',
-        desc    : 'Direccion de la ciudad para obtener el clima',
-        demand  : true
-    }
-}).argv;
-
-
-let encodedUrl = encodeURI(argv.direccion);
-
-axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyDSPDpkFznGgzzBSsYvTq_sj0T0QCHRgwM`)
+let encodeUrl = encodeURI(argv.direccion);
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeUrl}&key=AIzaSyDSPDpkFznGgzzBSsYvTq_sj0T0QCHRgwM`)
   .then( resp => {
+
       let location = resp.data.results[0];
       let coord = location.geometry.location;
 
