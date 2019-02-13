7486a34b061ab7576a7752f10d6207b7de23fe61
obbijuan <juan.garces@outlook.com>
2019-02-12 22:39:45 -0300

    fixed hbs

--- webserver/server.js
+++ webserver/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
 
 app.use(express.static(__dirname + '/public'))
 
+// Express hbs engine
+hbs.registerPartials( __dirname + '/views/partials');
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
