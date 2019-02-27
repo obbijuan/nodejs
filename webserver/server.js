12bc34de7fee50f56ea1c58ff10bdbf38d1c95b1
obbiejuan <juan.garces@outlook.com>
2019-02-27 14:58:28 -0300

    add about

--- webserver/server.js
+++ webserver/server.js
@@ -9,9 +9,15 @@ hbs.registerPartials( __dirname + '/views/partials');
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-
   res.render('home', {
       nombre:   'Obbijuan',
+      anio  :   new Date().getFullYear()
+  });
+
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
       anio  :   new Date().getFullYear()
   });
 
