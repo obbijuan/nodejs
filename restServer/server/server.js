58aedab2c4197fbf0f95318a630a79cfd38f4773
obbijuan <juan.garces@outlook.com>
2019-04-25 23:36:18 -0400

    Mongoose configurado

--- restServer/server/server.js
+++ restServer/server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -43,6 +45,14 @@ app.put('/usuario/:id', function(req, res){
 app.delete('/usuario', function(req, res){
     res.json('delete Usuario');
 })
+
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err, res)=>{
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
