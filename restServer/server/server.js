b04a800cc53f258ff042d50b4e98e964b82b0d54
obbijuan <juan.garces@outlook.com>
2019-04-28 00:50:07 -0400

    Ordenando rutas del usuario

--- restServer/server/server.js
+++ restServer/server/server.js
@@ -12,39 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// Importamos y usamos las rutas del usuario
+app.use(require('./routes/usuario');
 
-app.get('/usuario', function(req, res){
-    res.json('get Usuario');
-})
-
-app.post('/usuario', function(req, res){
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok:false,
-            mensaje:'El nombre es necesario!'
-        })
-    }else{
-        res.json({
-            persona: body
-        });
-    }
-
-
-})
-
-app.put('/usuario/:id', function(req, res){
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function(req, res){
-    res.json('delete Usuario');
-})
 
 mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err, res)=>{
 
